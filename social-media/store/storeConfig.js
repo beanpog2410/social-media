a5bfe752e1c7b1e38075f6aefe64687230aea714
Thuong <beanpog2410@gmail.com>
2022-01-01 21:18:50 +0700

    change feature

---
+++ social-media/store/storeConfig.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
